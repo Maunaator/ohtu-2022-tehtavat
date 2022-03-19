649850582cdcad27052496ed66238cd020d17044
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-03-19 03:43:00 +0200

    t.2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomli
 
 
 class ProjectReader:
@@ -9,7 +10,16 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+
+        sanak = tomli.loads(content)
+
+        nimi = sanak.get("tool").get("poetry").get("name")
+
+        kuvaus = sanak.get("tool").get("poetry").get("description")
+
+        riippuvuudet = sanak.get("tool").get("poetry").get("dependencies").keys()
+
+        devriippivuudet = sanak.get("tool").get("poetry").get("dev-dependencies").keys()
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(nimi, kuvaus, riippuvuudet, devriippivuudet)

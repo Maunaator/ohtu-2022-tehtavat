e0322a862fcbe58d4a3982425633022463462a01
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-03-27 15:13:53 +0300

    v.4 t.2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_korttia_ladataan_oikealla_summalla(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    def test_korttia_ei_ladata_jos_ladattava_summa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()

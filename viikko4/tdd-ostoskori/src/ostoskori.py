94e36faf12596444d92c47dc001ba57e06321b75
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-03-27 21:50:23 +0300

    4.5.8. ostokset metodi palauttaa listan ostoskorin sisallosta, dictionaryn uudelleen nimeaminen, peitti ostokset metodin

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,26 +3,26 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = {}
+        self.sisalto = {}
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        lukumaarat = map(lambda ostos: ostos.lukumaara(), self.ostokset.values())
+        lukumaarat = map(lambda ostos: ostos.lukumaara(), self.sisalto.values())
         return sum(lukumaarat)
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        hinnat = map(lambda ostos: ostos.hinta(), self.ostokset.values())
+        hinnat = map(lambda ostos: ostos.hinta(), self.sisalto.values())
         return sum(hinnat)
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        if self.ostokset.get(lisattava):
-            self.ostokset.get(lisattava).muuta_lukumaaraa(1)
+        if self.sisalto.get(lisattava):
+            self.sisalto.get(lisattava).muuta_lukumaaraa(1)
         else:
-            self.ostokset[lisattava] = Ostos(lisattava)
+            self.sisalto[lisattava] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -33,6 +33,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self.sisalto.values()
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on

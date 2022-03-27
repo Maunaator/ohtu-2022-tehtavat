d269eed0283d24632469e6a8344d7ed74a94e084
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-03-27 21:23:02 +0300

    4.5.5. testi hinnalle kahden tuotteen lisayksen jalkeen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -30,3 +30,11 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        kahvi = Tuote("Kahvi", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kahvi)
+
+        self.assertEqual(self.kori.hinta(), 8)
+

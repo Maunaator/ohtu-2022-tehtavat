f1bf0be202e1c4de6af59c8f39ffabfebdf86071
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-03-27 22:14:53 +0300

    4.5.11. kahden saman tuotteen lisayksen jalkeen korissa yksi ostos

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -71,8 +71,15 @@ class TestOstoskori(unittest.TestCase):
         kahvi = Tuote("Kahvi", 5)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(kahvi)
-        
+
         self.assertEqual(len(self.kori.ostokset()), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostos(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        
+        self.assertEqual(len(self.kori.ostokset()), 1)
 
 
 

0ae733acd3ed26ff5d433ab56275ba6dda19cadd
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-04-22 01:54:06 +0300

    self.m_score1 to self.player1_score

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,12 +2,12 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
+        self.player1_score = 0
         self.m_score2 = 0
 
     def won_point(self, player_name):
         if player_name == self.player1_name:
-            self.m_score1 += 1
+            self.player1_score += 1
         elif player_name == self.player2_name:
             self.m_score2 += 1
 
@@ -15,19 +15,19 @@ class TennisGame:
         score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
+        if self.player1_score == self.m_score2:
+            if self.player1_score == 0:
                 score = "Love-All"
-            elif self.m_score1 == 1:
+            elif self.player1_score == 1:
                 score = "Fifteen-All"
-            elif self.m_score1 == 2:
+            elif self.player1_score == 2:
                 score = "Thirty-All"
-            elif self.m_score1 == 3:
+            elif self.player1_score == 3:
                 score = "Forty-All"
             else:
                 score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        elif self.player1_score >= 4 or self.m_score2 >= 4:
+            minus_result = self.player1_score - self. m_score2
 
             if minus_result == 1:
                 score = "Advantage player1"
@@ -40,7 +40,7 @@ class TennisGame:
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.player1_score
                 else:
                     score = score + "-"
                     temp_score = self.m_score2

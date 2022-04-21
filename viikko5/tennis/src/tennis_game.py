d4023f81959d2f24e8cc6be23470154197f12c83
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-04-22 02:35:30 +0300

    points_to_point_names method, get_score remove repetition

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -11,21 +11,28 @@ class TennisGame:
         elif player_name == self.player2_name:
             self.player2_score += 1
 
+    def _points_to_point_names(self, points):
+        if points == 0:
+            return "Love"
+        elif points == 1:
+            return "Fifteen"
+        elif points == 2:
+            return "Thirty"
+        elif points == 3:
+            return "Forty"
+        else:
+            return "Invalid"
+
     def get_score(self):
         score = ""
         temp_score = 0
 
         if self.player1_score == self.player2_score:
-            if self.player1_score == 0:
-                score = "Love-All"
-            elif self.player1_score == 1:
-                score = "Fifteen-All"
-            elif self.player1_score == 2:
-                score = "Thirty-All"
-            elif self.player1_score == 3:
-                score = "Forty-All"
-            else:
+            if self.player1_score == 4:
                 score = "Deuce"
+            else:
+                score = self._points_to_point_names(self.player1_score) + "-All"
+
         elif self.player1_score >= 4 or self.player2_score >= 4:
             minus_result = self.player1_score - self.player2_score
 
@@ -37,6 +44,7 @@ class TennisGame:
                 score = "Win for player1"
             else:
                 score = "Win for player2"
+            
         else:
             for i in range(1, 3):
                 if i == 1:
@@ -45,13 +53,6 @@ class TennisGame:
                     score = score + "-"
                     temp_score = self.player2_score
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+                score = score + self._points_to_point_names(temp_score)
 
         return score

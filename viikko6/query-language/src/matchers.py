d57b61237532be4d7e7f218642b4391eb6988567
Maunaator <78967619+Maunaator@users.noreply.github.com>
2022-04-27 21:55:35 +0300

    6.2

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,23 @@ class And:
         
         return True
 
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+    
+    def matches(self, player):
+        if self._matcher.matches(player):
+            return False
+        else:
+            return True
+
+class All:
+    def __init__(self):
+        pass
+
+    def matches(self, player):
+        if player: return True
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -25,3 +42,13 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
